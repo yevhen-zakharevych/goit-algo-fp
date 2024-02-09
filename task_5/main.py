d7828d51ed259ec37cadc5f5a91e84e62e61c7ef
Yevhen Zakharevych <yzakharevych@fieldcomplete.com>
2024-02-09 14:56:44 +0800

    task_5

---
+++ task_5/main.py
@@ -0,0 +1,113 @@
+import uuid
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.colors as mc
+import colorsys
+
+
+def lighten_color(color, amount=0.8):
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
+
+
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
+
+
+def dfs(node):
+    color = '#1296F0'
+
+    def _dfs(node):
+        if node is None:
+            return
+
+        nonlocal color
+        color = lighten_color(color)
+        node.color = color
+
+        _dfs(node.left)
+        _dfs(node.right)
+
+    _dfs(node)
+
+
+def bfs(node):
+    if node is None:
+        return
+
+    queue = [node]
+    color = '#1296F0'
+
+    while queue:
+        next_level = []
+        for n in queue:
+            color = lighten_color(color)
+            n.color = color
+
+            if n.left:
+                next_level.append(n.left)
+            if n.right:
+                next_level.append(n.right)
+        queue = next_level
+
+
+if __name__ == "__main__":
+    root = Node(0)
+    root.left = Node(4)
+    root.left.left = Node(5)
+    root.left.right = Node(10)
+    root.right = Node(1)
+    root.right.left = Node(3)
+
+    dfs(root)
+    draw_tree(root)
+
+    root = Node(0)
+    root.left = Node(4)
+    root.left.left = Node(5)
+    root.left.right = Node(10)
+    root.right = Node(1)
+    root.right.left = Node(3)
+
+    bfs(root)
+    draw_tree(root)
